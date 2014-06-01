78a1acfd791fd61184db1d8253117567d374bcf3
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2014-06-01 14:21:57 -0500

    * drop target

---
+++ src/TDDropTargetView.m
@@ -0,0 +1,158 @@
+//
+//  DropTargetView.m
+//  PrivateAlly
+//
+//  Created by Todd Ditchendorf on 4/29/14.
+//  Copyright (c) 2014 Todd Ditchendorf. All rights reserved.
+//
+
+#import <TDAppKit/TDDropTargetView.h>
+#import <TDAppKit/TDHintButton.h>
+
+#define MARGIN 60.0
+
+#define ROUND_RADIUS 10.0
+
+#define SPACE_LEN 8.0
+#define IDEAL_DASH_LEN 40.0
+
+#define HINT_MIN_WIDTH 50.0
+#define HINT_HEIGHT 100.0
+#define HINT_MARGIN_X 100.0
+
+@interface NSObject ()
+- (void)browse:(id)sender;
+@end
+
+@implementation TDDropTargetView
+
+- (instancetype)initWithFrame:(NSRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        CGRect r = [self hintButtonRectForBounds:[self bounds]];
+        self.hintButton = [[[TDHintButton alloc] initWithFrame:r] autorelease];
+        [_hintButton setAutoresizingMask:NSViewWidthSizable|NSViewHeightSizable];
+        [_hintButton setHintText:NSLocalizedString(@"Add an XML document.", @"")];
+        [self addSubview:_hintButton];
+    }
+    return self;
+}
+
+
+- (void)dealloc {
+    self.hintButton = nil;
+    [super dealloc];
+}
+
+
+#pragma mark -
+#pragma mark NSDraggingDestination
+
+- (NSDragOperation)draggingEntered:(id <NSDraggingInfo>)dragInfo {
+    TDAssert([[[self window] windowController] document]);
+    return [[[[self window] windowController] document] draggingEntered:dragInfo];
+}
+
+
+- (BOOL)performDragOperation:(id <NSDraggingInfo>)dragInfo {
+    TDAssert([[[self window] windowController] document]);
+    BOOL success = [[[[self window] windowController] document] performDragOperation:dragInfo];
+    return success;
+}
+
+
+#pragma mark -
+#pragma mark TDView
+
+- (void)layoutSubviews {
+    TDAssertMainThread();
+    TDAssert(_hintButton);
+    
+    CGRect bounds = [self bounds];
+    _hintButton.frame = [self hintButtonRectForBounds:bounds];
+}
+
+
+#pragma mark -
+#pragma mark NSView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    CGContextRef ctx = [[NSGraphicsContext currentContext] graphicsPort];
+    
+    CGRect bounds = [self bounds];
+    [[NSColor windowBackgroundColor] setFill];
+    NSRectFill(bounds);
+    
+    CGFloat margin = MARGIN;
+    CGRect r = CGRectInset(bounds, margin, margin);
+    
+    CGContextSetLineWidth(ctx, 4.0);
+    CGContextSetGrayStrokeColor(ctx, 0.67, 1.0);
+
+    CGFloat minx = CGRectGetMinX(r);
+    CGFloat midx = CGRectGetMidX(r);
+    CGFloat maxx = CGRectGetMaxX(r);
+    CGFloat miny = CGRectGetMinY(r);
+    CGFloat midy = CGRectGetMidY(r);
+    CGFloat maxy = CGRectGetMaxY(r);
+
+    CGFloat height = r.size.height - ROUND_RADIUS;
+    CGFloat width = r.size.width - ROUND_RADIUS;
+
+//    CGFloat yNumSegs = round(height/IDEAL_DASH_LEN);
+//    CGFloat xNumSegs = round(width/IDEAL_DASH_LEN);
+
+    CGFloat yNumSegs = 18.0;
+    CGFloat xNumSegs = 10.0;
+
+    CGFloat ySegLen = height/yNumSegs;
+    CGFloat yDashLen = ySegLen - SPACE_LEN;
+    
+    CGFloat xSegLen = width/xNumSegs;
+    CGFloat xDashLen = xSegLen - SPACE_LEN;
+
+    CGFloat yLens[] = {yDashLen, SPACE_LEN};
+    CGFloat xLens[] = {xDashLen, SPACE_LEN};
+
+    CGFloat yPhase = yDashLen*0.5;
+    CGFloat xPhase = xDashLen*0.5;
+    
+    CGContextBeginPath(ctx);
+    CGContextMoveToPoint(ctx, minx, maxy-ROUND_RADIUS);
+    CGContextAddArcToPoint(ctx, minx, miny, midx, miny, ROUND_RADIUS);
+    CGContextSetLineDash(ctx, yPhase, yLens, 2);
+    CGContextStrokePath(ctx);
+
+    CGContextBeginPath(ctx);
+    CGContextMoveToPoint(ctx, minx+ROUND_RADIUS, miny);
+    CGContextAddArcToPoint(ctx, maxx, miny, maxx, midy, ROUND_RADIUS);
+    CGContextSetLineDash(ctx, xPhase, xLens, 2);
+    CGContextStrokePath(ctx);
+
+    CGContextBeginPath(ctx);
+    CGContextMoveToPoint(ctx, maxx, miny+ROUND_RADIUS);
+    CGContextAddArcToPoint(ctx, maxx, maxy, midx, maxy, ROUND_RADIUS);
+    CGContextSetLineDash(ctx, yPhase, yLens, 2);
+    CGContextStrokePath(ctx);
+    
+    CGContextBeginPath(ctx);
+    CGContextMoveToPoint(ctx, maxx-ROUND_RADIUS, maxy);
+    CGContextAddArcToPoint(ctx, minx, maxy, minx, midy, ROUND_RADIUS);
+    CGContextSetLineDash(ctx, xPhase, xLens, 2);
+    CGContextStrokePath(ctx);
+}
+
+
+- (CGRect)hintButtonRectForBounds:(CGRect)bounds {
+    CGFloat w = round(CGRectGetWidth(bounds) - HINT_MARGIN_X*2.0);
+    w = MAX(HINT_MIN_WIDTH, w);
+    CGFloat h = HINT_HEIGHT;
+
+    CGFloat x = round(CGRectGetWidth(bounds)*0.5 - w*0.5);
+    CGFloat y = round(CGRectGetHeight(bounds)*0.5 - h*0.5);
+    
+    CGRect r = CGRectMake(x, y, w, h);
+    return r;
+}
+
+@end
---
+++ src/TDHintButton.m
@@ -0,0 +1,132 @@
+//
+//  TDHintView.m
+//  TDAppKit
+//
+//  Created by Todd Ditchendorf on 11/11/10.
+//  Copyright 2010 Todd Ditchendorf. All rights reserved.
+//
+
+#import <TDAppKit/TDHintButton.h>
+#import <TDAppKit/TDUtils.h>
+
+#define HINT_MIN_WIDTH 100.0
+#define HINT_MAX_WIDTH 300.0
+
+#define HINT_HEIGHT 42.0
+#define HINT_MARGIN_X 0.0
+#define HINT_PADDING_X 22.0
+#define HINT_PADDING_Y 14.0
+
+#define HINT_VERT_FUDGE 0.0
+
+static NSColor *sHintBgColor = nil;
+static NSColor *sHintHiBgColor = nil;
+static NSDictionary *sHintAttrs = nil;
+
+@implementation TDHintButton
+
++ (void)initialize {
+    if ([TDHintButton class] == self) {
+        
+        //[[NSColor colorWithDeviceWhite:.87 alpha:1] set];
+        //[[NSColor colorWithDeviceRed:230.0/255.0 green:236.0/255.0 blue:242.0/255.0 alpha:1] set];
+        
+        sHintBgColor = [[NSColor colorWithDeviceWhite:0.68 alpha:1.0] retain];
+        sHintHiBgColor = [[NSColor colorWithDeviceWhite:0.58 alpha:1.0] retain];
+        
+        NSMutableParagraphStyle *paraStyle = [[[NSParagraphStyle defaultParagraphStyle] mutableCopy] autorelease];
+        [paraStyle setAlignment:NSCenterTextAlignment];
+        [paraStyle setLineBreakMode:NSLineBreakByWordWrapping];
+        
+        NSShadow *shadow = [[[NSShadow alloc] init] autorelease];
+        [shadow setShadowColor:[NSColor colorWithDeviceWhite:0.0 alpha:0.5]];
+        [shadow setShadowOffset:NSMakeSize(0.0, -1.0)];
+        [shadow setShadowBlurRadius:2.0];
+        
+        sHintAttrs = [[NSDictionary alloc] initWithObjectsAndKeys:
+                      [NSFont boldSystemFontOfSize:14.0], NSFontAttributeName,
+                      [NSColor whiteColor], NSForegroundColorAttributeName,
+                      shadow, NSShadowAttributeName,
+                      paraStyle, NSParagraphStyleAttributeName,
+                      nil];
+    }
+}
+
+
+- (instancetype)initWithFrame:(NSRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setButtonType:NSMomentaryChangeButton];
+        [self setFocusRingType:NSFocusRingTypeNone];
+        [self setTag:0];
+    }
+    return self;
+}
+
+
+- (void)dealloc {
+    self.hintText = nil;
+    [super dealloc];
+}
+
+
+- (NSString *)description {
+    return [NSString stringWithFormat:@"<%@ %p '%@'>", [self class], self, _hintText];
+}
+
+
+- (NSRect)hintTextRectForBounds:(NSRect)bounds {
+    CGFloat w = bounds.size.width - HINT_MARGIN_X * 2 - HINT_PADDING_X * 2;
+    w = w < HINT_MIN_WIDTH ? HINT_MIN_WIDTH : w;
+    
+    NSRect strRect = [_hintText boundingRectWithSize:NSMakeSize(w, 10000000.0) options:NSStringDrawingUsesLineFragmentOrigin attributes:sHintAttrs];
+
+    CGFloat h = strRect.size.height;
+    CGFloat x = HINT_MARGIN_X + HINT_PADDING_X;
+    CGFloat y = bounds.size.height / 2 - strRect.size.height / 2 + HINT_VERT_FUDGE;
+    y += _hintTextOffsetY;
+
+    NSRect r = NSMakeRect(x, y, w, h);
+    return r;
+}
+
+
+- (void)drawRect:(NSRect)dirtyRect {
+    NSRect bounds = [self bounds];
+
+//    [[NSColor redColor] setFill];
+//    NSRectFill(bounds);
+    
+    BOOL isHi = [[self cell] isHighlighted]; //NSOnState == [self state];
+    
+    BOOL showHint = ([_hintText length]);
+    if (showHint) {
+        NSRect hintTextRect = [self hintTextRectForBounds:bounds];
+        
+        NSRect hintRect = NSInsetRect(hintTextRect, -HINT_PADDING_X, -HINT_PADDING_Y);
+        
+        CGFloat w = hintRect.size.width;
+        w = w > HINT_MAX_WIDTH ? HINT_MAX_WIDTH : w;
+        hintRect.size.width = floor(w);
+        
+        CGFloat x = bounds.size.width / 2 -  hintRect.size.width / 2;
+        x = x < HINT_MARGIN_X ? HINT_MARGIN_X : x;
+        hintRect.origin.x = floor(x);
+        
+        hintRect.origin.y = floor(hintRect.origin.y);
+        hintRect.size.height = floor(hintRect.size.height);
+        CGFloat radius = hintRect.size.height / 2 - 2;
+        
+        NSColor *bgColor = isHi ? sHintHiBgColor : sHintBgColor;
+        [bgColor setFill];
+        
+        CGContextRef ctx = [[NSGraphicsContext currentContext] graphicsPort];
+        TDAddRoundRect(ctx, hintRect, radius);
+        CGContextFillPath(ctx);
+        
+        [[NSColor whiteColor] set];
+        [_hintText drawInRect:hintTextRect withAttributes:sHintAttrs];
+    }
+}
+
+@end
